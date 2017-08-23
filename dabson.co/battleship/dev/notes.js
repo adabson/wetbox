61f57bfed4581feb4e34c89a1398d7e584715584
Andrew Dabson <adabson@hotmail.com>
2017-08-23 19:58:39 +1000

    battleship

---
+++ dabson.co/battleship/dev/notes.js
@@ -0,0 +1,7 @@
+	/*
+	addShip( container, hitbox1, 5, 1, 1, true );
+	addShip( container, hitbox1, 2, 7, 8, false );
+	addShip( container, hitbox1, 4, 7, 2, true );
+	addShip( container, hitbox1, 3, 3, 3, false );
+	*/
+
