cd875aa9dd3f20dbb0cf8f922fe9428c8efab159
Dabson <cyphlix@gmail.com>
2018-07-28 14:13:26 +1000

    Add portfolio site from ages ago

---
+++ dabson.co/andrew/index.php
@@ -0,0 +1,124 @@
+<html>
+<head>
+  <title>Dabson Design</title>
+  <link rel="shortcut icon" href="images/icon.ico" type="image/x-icon">
+
+  <META name="distribution" content="global">
+  <META name="resource-type" content="document">
+  <META name="description" content="Dabson design: Andrew dabson online portfolio">
+  <META name="keywords" content="cg graphic, design, 3ds, max, photoshop, 
+                             tutorial, flash, swf, download, resource, forum, muzo, develop, developer, 
+                             designer, program, programmer, artist, mastery, Andrew Dabson, mess, pad, messpad,
+                             virtual notepad, notepad, notes, create, creator, creative">
+
+  <script type="text/javascript" src="js/prototype.js"></script>
+  <script type="text/javascript" src="js/scriptaculous.js?load=effects,builder"></script>
+  <script type="text/javascript" src="js/lightbox.js"></script>
+  <script type="text/javascript">function fullwin(){window.open("index.html","bfs","fullscreen,scrollbars")}</script>
+
+  <script type="text/javascript">
+    pic1=new Image(500,500); 
+    pic1.src="images/stars.gif";
+    pic2=new Image(750,750); 
+    pic2.src="images/sun.png";
+  </script>
+
+  <link rel="stylesheet" href="css/main.css" type="text/css" media="screen" />
+  <link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+</head>
+
+<body>
+
+<div class="sun"></div>
+<div class="cbg"></div>
+<div class="header"><br /><h1>Andrew Dabson</h1><h2>Digital design portfolio</h2><h3>Hello &amp; welcome to my star.</h3>
+
+</div>
+<div class="content">Currently a 3rd year graphic design student, I'm an Australian who's primary passions include web <br />design, and 3D-CG design. 
+<br /><br />While designing, I always opt to prioritise function over form. <br />
+I'm a fan of concise design <br />that is beautiful and to-the-point. 
+<br /><br />Difference and superiority must be bold. It must be targeted. And like any revolutionary idea, <br />it must take risks.
+<br /><br />Convention is creativity's worst enemy.</div>
+
+  <div class="gallery">
+  <a href="images/showcase/Andrew_Dabson-1_enig_Font.gif" rel="lightbox[folio]" name="Enig font" 
+  title="The font Enig (abbreviated: enigma) Is a mono-spaced, round serif font intended for use in programming and body-print text. The font was part of a major project in my 3rd year uni folio and took a considerable amount of time to perfect.">
+  <img src="images/showcase/Andrew_Dabson-1_enig_Font_thumb.gif" alt="Andrew Onitz Dabson Enig font" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-2_edwards_logo.gif" rel="lightbox[folio]" name="Edwards Logo" 
+  title="Edwards solar hot water systems is an existing Australian company, after being given a mock-brief to redesign their exiting logo, my solution featured a concentric E rippling from the upper surface of the letterform.">
+  <img src="images/showcase/Andrew_Dabson-2_edwards_logo_thumb.gif" alt="Andrew Onitz Dabson Edwards Logo" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-3_cool_book.jpg" rel="lightbox[folio]" name="Cool hotels cool prices book cover" 
+  title="The design for the book cover: Cool Hotels, Cool prices. Intended as a budget-saver for holiday makers, the intent behind the design was to create the cover in a manner that catches viewers attention in store and to stand out among the shelves. Although aimed at budget sellers, it does not necessarily have to be tacky.">
+  <img src="images/showcase/Andrew_Dabson-3_cool_book_thumb.gif" alt="Andrew Onitz Dabson Cool hotels cool prices book cover" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-4_droste.jpg" rel="lightbox[folio]" name="Droste effect cover packaging" 
+  title="The Droste effect is a prolific graphic anomaly where an infinitely recurring image is shown (one inside the other, inside the other etc). The original package,  introduced in 1904 was a picture of a nurse holding a package with another picture of a nurse and so on. Although this was a hard product to redesign, the end result still contained the concept, only in a more modern fashion.">
+  <img src="images/showcase/Andrew_Dabson-4_droste_thumb.gif" alt="Andrew Onitz Dabson Droste effect cover packaging" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-5_nier_logo.png" rel="lightbox[folio]" name="NIER Newcastle Institute for Energy and resources logo" 
+  title="The purpose behind this brief was to create a corporate identity in the form of a logo for the Newcastle Institute of Energy and Resources (NIER). The logo was to run along the lines of representing aspects of research, clean energy and resources, and/or environmental / social benefits. The meaning this design was ecological prosperity through research. The benzene ring is both an eminent chemical sign as well as a clearly-recognizable shape delivering a strong result. The line entering from the left and exiting / sprouting out the right over a bed of soil entails the research process as moving forward (as read from left to right).">
+  <img src="images/showcase/Andrew_Dabson-5_nier_thumb.gif" alt="Andrew Onitz Dabson NIER Newcastle Institute for Energy and resources logo" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-6_ambigram.png" rel="lightbox[folio]" name="Cogito ergo sum I think thus I am ambigram" 
+  title="The ambigram is a quote from the famous 17th century French philosopher and mathematician, Rene Descartes (pronounced des-cart) . On one side, the phrase reads cogito ergo sum, while on the other, its English translation: i think thus, i am (thus being a substitution for therefore to unify character count). The original brief was to design a tattoo for yourself. I chose to do an ambigram of this quote because I desired a deeper meaning to accompany something beautiful to etch into my skin.">
+  <img src="images/showcase/Andrew_Dabson-6_ambigram_thumb.gif" alt="Andrew Onitz Dabson Cogito ergo sum I think thus I am ambigram" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-7_freelancer.png" rel="lightbox[folio]" name="Freelancer logo" 
+  title="The freelancer logo was designed as part of a competition for the site: freelancer.com. The logo had to work in 2 colors (black and white). I designed it with the intent of portraying networking (the three stems coming from a single source), while also recognizing that a freelancer is usually only just one step in the problem-solution process. A drop in the ocean, so to speak.">
+  <img src="images/showcase/Andrew_Dabson-7_freelancer_thumb.gif" alt="Andrew Onitz Dabson Freelancer logo" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-8_3dsmax_boat.jpg" rel="lightbox[folio]" name="3ds max boat" 
+  title="This image was created solely as a hobby in 3ds max. The end result was more or less a consequence of a series of lighting experiments which rendered some rather gorgeous results. The overall cleanliness and lightness of detail, while unrealistic delivered a unique style which I personally found aesthetically pleasing and good enough to include in this folio.">
+  <img src="images/showcase/Andrew_Dabson-8_3dsmax_boat_thumb.gif" alt="Andrew Onitz Dabson 3ds max boat" class="lbl" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+    <br />
+
+  <a href="images/showcase/Andrew_Dabson-9_c4d_caustics_bunny.jpg" rel="lightbox[folio]" name="c4d caustics sphinx bunny rabbit" 
+  title="The sphinx bunny was a result of using translucent shaders and surface caustics in cinema 4d. The light patterns formed by the caustics create a unique effect which is both intriguing and different in its own right.">
+  <img src="images/showcase/Andrew_Dabson-9_c4d_caustics_bunny_thumb.gif" alt="Andrew Onitz Dabson c4d caustics sphinx bunny rabbit" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-10_3dsmax_corbusier_chair.jpg" rel="lightbox[folio]" name="3ds max corbusier chair" 
+  title="The Corbusier chair is a designer lounge which I self-modeled and surfaced using a texturing tutorial on 3ds max. The render time was noticeably long and used a lot of tricks in the vRay rendering engine to achieve near-photorealism.">
+  <img src="images/showcase/Andrew_Dabson-10_3dsmax_corbusier_chair_thumb.gif" alt="Andrew Onitz Dabson 3ds max corbusier chair" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-11_ebookjunkies_logo.gif" rel="lightbox[folio]" name="ebook junkies logo" 
+  title="This was the award-winning logo for an online community competition. Made a while back, there are a number of revisions I would make now (most immediate removing the gradient). Im very much still fond of the type however, and have chosen to include the logo in its entirety.">
+  <img src="images/showcase/Andrew_Dabson-11_ebookjunkies_logo_thumb.gif" alt="Andrew Onitz Dabson ebook junkies logo" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-12_scifi_cell.jpg" rel="lightbox[folio]" name="Sci-fi cg computer generated cell" 
+  title="Made from a tutorial titled: Karamendos cell animation, this image was created to include specific reflective and self-luminescent surfaces in a virtual 3D environment.">
+  <img src="images/showcase/Andrew_Dabson-12_scifi_cell_thumb.gif" alt="Andrew Onitz Dabson Sci-fi cg computer generated cell" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-13_snowflake.png" rel="lightbox[folio]" name="Ice Snowflake" 
+  title="This graphic was a discarded piece in my solution to the cool hotels, cool prices book cover (see 3). It was ultimately rendered with about 6 omni lights illuminating the stems from varying directions to produce a cool, crystal effect. Unfortunately, it looks much better on screen then it does on print making it unfeasible for the brief, but striking nonetheless.">
+  <img src="images/showcase/Andrew_Dabson-13_snowflake_thumb.gif" alt="Andrew Onitz Dabson Ice Snowflake" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-14_vector_headphones.png" rel="lightbox[folio]" name="Vector Headphones" 
+  title="A product illustration for a pair of RadioShack wireless headphones. Everything was manually hand-illustrated with the pen tool, gradient ramps and various other tools and effects in Adobe illustrator (ear pads were auto-traced). As a  final piece, I was very satisfied with this work (huge time investment for intricate detail).">
+  <img src="images/showcase/Andrew_Dabson-14_vector_headphones_thumb.gif" alt="Andrew Onitz Dabson Vector Headphones" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-15_box_octapus.png" rel="lightbox[folio]" name="vRay box octapus voxelation" 
+  title="Voxellation is a process similar to pixilation, but in 3d whereby an image can be mapped using cubes (voxels) instead of squares (pixels). This representation  doesn't necessarily reflect the concept, rather is intended as a fun, cute and creepy background image. The font used is Rabid Science.">
+  <img src="images/showcase/Andrew_Dabson-15_box_octapus_thumb.gif" alt="Andrew Onitz Dabson vRay box octapus voxelation" class="lb" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+
+  <a href="images/showcase/Andrew_Dabson-16_sunset_surf.png" rel="lightbox[folio]" name="Sunset surf" 
+  title="Created using 3ds max in a manner similar to the boat scene, but with less detail water surface textures and lighting. The picture is a piece of fine art more than anything else. Not seeking excessive beauty or realism, this is an image of relaxation, abstraction and simplicity.">
+  <img src="images/showcase/Andrew_Dabson-16_sunset_surf_thumb.gif" alt="Andrew Onitz Dabson Sunset surf" class="lbl" style="opacity:0.4;filter:alpha(opacity=40)" onmouseover="this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout="this.style.opacity=0.4;this.filters.alpha.opacity=40" /></a>
+  </div>
+
+
+<div class="footer">
+  <!-- The following integrates facebook's like feature by button liking onitz.net width:450 style: dark bg -->
+  <iframe src="https://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.onitz.net%2F&amp;layout=button_count&amp;show_faces=true&amp;width=450&amp;action=like&amp;font=verdana&amp;colorscheme=dark&amp;height=21" id="fb" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+  <div id="legal">&copy; 2010-<script>document.write((new Date()).getFullYear());</script> Dabson</div>
+
+  <a href="mailto:cyphlix@hotmail.com?subject=Design%20inquiry" title="Design inquiry"><button id="contact" onmouseover="className='buttonhover';" onmouseout="className='button';">Contact</button></a>
+
+  <a href="https://www.linkedin.com/in/andrew-dabson-0102b315/" target="_blank"><button id="resume" onmouseover="className='buttonhover';" onmouseout="className='button';">LinkedIn</button></a>
+  <button id="max" onClick="fullwin()" onmouseover="className='buttonhover';" onmouseout="className='button';">Expand</button>
+</div>
+
+</body>
+</html>
