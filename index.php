38daf372f51fdf38586c56e0836eb53cae70f2cd
Dabson <cyphlix@gmail.com>
2018-07-29 23:46:11 +1000

    fix that shizz

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $conn = new mysqli( 'localhost', 'username', 'password', 'base' );
 if ( $conn->connect_error ) {
   die();
 }
-$sql = 'select * from (select * from uptime order by date desc limit 1440) as lastDay order by lastDay.date asc';
+$sql = 'select * from (select * from uptime order by date desc limit 1441) as lastDay order by lastDay.date asc';
 $results = $conn->query($sql);
 $conn->close();
 ?>
@@ -39,7 +39,8 @@ h1{
 }
 .col {
   width:25px;
-  display:inline-block;
+  height:660px;
+  display:block;
   float:left;
 }
 .nicetime {
@@ -53,49 +54,36 @@ h1{
 <body>
 
 <div style="margin-bottom:50px;">
-<h1 style="float:left;margin-right:10px">Server Status (24hr)</h1>
-<div class="box" style="float:left;"></div>   = blackout<br>
-<div class="box g" style="float:left;"></div> = online<br>
-<div class="box r" style="float:left;"></div> = offline
+  <h1 style="float:left;margin-right:10px">Server Status (24hr)</h1>
+  <div class="box" style="float:left;"></div> = blackout<br>
+  <div class="box g" style="float:left;"></div> = online<br>
+  <div class="box r" style="float:left;"></div> = offline
 </div>
+<br>
 
 <div class="col">
 <?
-$lastUnixTime = -1;
-$coli = 0;
-while($row = $results->fetch_assoc()) {
-  $unixTime = strtotime( $row["date"] ); //current unix time should be 60secs less than last entry cause its desc
-  $expected = $unixTime - 60;
+$nowTick = (time()/60) % 1440;
+$shift = 1399 - $nowTick;
+$firstrow = $results->fetch_assoc()["date"];
+$firstTick = (strtotime( $firstrow )/60 + $shift) % 1440;
 
-  if( $lastUnixTime == -1 ) { //create filler
-    echo '<div style="display:block;height:'.( 22 + ($unixTime%3600)*11/60 ).'px"></div>';
-  } elseif( $lastUnixTime != -1 && $lastUnixTime != $expected ) {
-    //ensure that both are on-the-minute stamps and delta does not exceed 100 blackout minutes (6000 seconds)
-    $del = $expected -$lastUnixTime;
-    newcol( ++$coli );
-
-
-    if( $del < 6000 ) {
-      for( $i = 0; $i < floor($del/60); $i++ ) {
-        newCol( ++$coli );
-        echo "<div class='box'></div>";
-      }
-    } else {
-      echo "<div class='box'></div> ... ".($del/3600)." hour blackout ... <div class='box'></div> ";
-    }
-  }
-  $lastUnixTime = $unixTime;
-  newCol( ++$coli );
-
-  $onoff = $row["isup"] == 1 ? 'g' : 'r';
-  $niceTime = date( 'h:ia', strtotime( $row["date"] ) );
-  echo "<div class='box $onoff'></div>";
+for($i=0;$i<$firstTick;$i++) {
+  newCol($i);
+  echo '<div class="box"></div>';
 }
 
-function newCol( $coli ) {
-  $cond = $coli % 60 == 0;
+for(;$row=$results->fetch_assoc();) {
+  $tock = (strtotime( $row["date"] )/60 + $shift) % 1440; //rounded to the nearest minute //1440 mins in a day
+  $onoff = $row["isup"] == 1 ? 'g' : 'r';
+  echo "<div class='box $onoff'></div>";
+  newCol($tock);
+}
+
+function newCol( $x ) {
+  $cond = $x % 60 == 0;
   if ( $cond ) {
-    echo '</div><div class="col"><span class="nicetime"></span>';
+    echo '</div><div class="col"><span class="nicetime">'.($x/60).'</span>';
   }
 }
 ?>
