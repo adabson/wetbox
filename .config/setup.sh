94b3e3f69b28bd85484fd520e64b32e12e4850ab
Dabson <cyphlix@gmail.com>
2018-07-29 19:09:03 +1000

    hook works?

--- .config/setup.sh
+++ .config/setup.sh
@@ -2,6 +2,8 @@
 # Requires git
 CRONTAB="* * * * * php /volume1/Web/.config/cron/minutly.php"
 BARE="/volume1/Public/wetbox.git"
+POSTRECEIVEHOOK="/volume1/Public/wetbox.git/hooks/post-receive"
+POSTRECEIVE="TARGET=\"/volume1/Web\" GIT_DIR=\"/volume1/Public/wetbox.git\" BRANCH="master" while read oldrev newrev ref do if [[ $ref = refs/heads/$BRANCH ]]; then echo \"Ref $ref received. Deploying ${BRANCH} branch to production...\" git --work-tree=$TARGET --git-dir=$GIT_DIR checkout -f else echo \"Ref $ref received. Doing nothing: only the ${BRANCH} branch may be deployed on this server.\" fi done"
 
 # install git 
 # clone bare repo 
@@ -13,20 +15,28 @@ BARE="/volume1/Public/wetbox.git"
 echo "Checking out bare repo..."
 if [ -d $BARE ]; 
 then
-   echo "  Bare $BARE exists."
+   echo "  Bare exists."
 else
   git clone --bare https://github.com/Onitz/wetbox.git $BARE
   echo "  Checked out bare git repo at $BARE"
 fi
 
-# Add auto-deploy hook to bare 
-
+# Add auto-deploy hook to bare
+echo "Adding auto-deploy hook to bare repo..."
+if [ -f $POSTRECEIVEHOOK ]; 
+then
+   echo "  Hook exists."
+else
+  #git clone --bare https://github.com/Onitz/wetbox.git $BARE
+  echo $POSTRECEIVE >> $POSTRECEIVEHOOK
+  echo "  Added post-receive (auto-deploy) hook at $POSTRECEIVEHOOK"
+fi
 
 # Add the minutely cron entry
 echo "Writing cron entry (run php minutely.php every minute)..." 
 if crontab -l | grep -q "$CRONTAB";
 then
-  echo "  Cron entry exists";
+  echo "  Cron entry exists.";
 else
   (crontab -l ; echo "* * * * * php /volume1/Web/.config/cron/minutly.php") | crontab -
   echo "  Wrote cron entry!"
