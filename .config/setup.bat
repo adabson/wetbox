a796358a7aa97c4be9821721ee4adb8e08823b5f
Dabson <cyphlix@gmail.com>
2018-07-29 18:54:43 +1000

    good progress

--- .config/setup.bat
+++ .config/setup.bat
@@ -1,1 +1,5 @@
+@echo off
+REM literally just a wrapper to run the setup.sh script on the remote server
+@echo on
+
 plink root@wetbox -m setup.sh
--- .config/setup.sh
+++ .config/setup.sh
@@ -1,20 +1,34 @@
 ##!/usr/bin
+# Requires git
 CRONTAB="* * * * * php /volume1/Web/.config/cron/minutly.php"
+BARE="/volume1/Public/wetbox.git"
 
 # install git 
 # clone bare repo 
 # add auto-deploy hook: bare/hooks/post-receive 
 # chmod +x post-receive
-# checkout working dir 
+# checkout working dir
 
-# create database user/pass 
+# Checkout bare to host repo on the server
+echo "Checking out bare repo..."
+if [ -d $BARE ]; 
+then
+   echo "  Bare $BARE exists."
+else
+  git clone --bare https://github.com/Onitz/wetbox.git $BARE
+  echo "  Checked out bare git repo at $BARE"
+fi
+
+# Add auto-deploy hook to bare 
+
+
+# Add the minutely cron entry
 echo "Writing cron entry (run php minutely.php every minute)..." 
 if crontab -l | grep -q "$CRONTAB";
 then
-  echo 'Entry exists';
+  echo "  Cron entry exists";
 else
-  #echo 'entry not exists';
   (crontab -l ; echo "* * * * * php /volume1/Web/.config/cron/minutly.php") | crontab -
+  echo "  Wrote cron entry!"
 fi
-echo "done!"
 # chmod 770 flat santa
