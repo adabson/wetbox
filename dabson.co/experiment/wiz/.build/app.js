df481869c8a878217e24c8a9b976dac786de5a11
Dabson <cyphlix@gmail.com>
2018-08-06 12:50:10 +1000

    meaningless

--- dabson.co/experiment/wiz/.build/app.js
+++ dabson.co/experiment/wiz/.build/app.js
@@ -1,6 +1,25 @@
-//let x = require('./x.js');
+//yes, i want this global
+wet = require( '../../../../../npm-wetbox' ); // @todo: replace this with legit wetbox installation
+
 document.title = 'wiz';
-//let body = document.getElementsByTagName('html')[0].appendElement( 'body')
+
+console.log( 'Using version:', wet.getVersion() );
+wet.keyListen( 
+  () => { return true },
+  () => { console.log( 'up' ) }, 
+  () => { console.log( 'down' ) }, 
+  () => { console.log( 'left' ) }, 
+  () => { console.log( 'right' ) }, 
+  () => { console.log( 'space' ) }, 
+  () => { console.log( 'ctrl' ) } 
+);
+
+//console.log(d)
+
 let canvasEl = document.createElement('canvas');
 canvasEl.setAttribute('id', 'canvas');
 document.body.appendChild( canvasEl );
+
+console.log(wet);
+
+let x='xxxxx';
