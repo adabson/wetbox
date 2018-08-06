b121cf9283133f01e5faf1dc01b340e72eac72bb
Dabson <cyphlix@gmail.com>
2018-08-06 19:45:11 +1000

    touch up that wizzy boi

--- dabson.co/experiment/wiz/.build/app.js
+++ dabson.co/experiment/wiz/.build/app.js
@@ -1,25 +1,10 @@
-//yes, i want this global
-wet = require( '../../../../../npm-wetbox' ); // @todo: replace this with legit wetbox installation
+let WET = window.WET
+try {
+  WET = window.WET = require( '../../../../../npm-wetbox' ) // @todo: replace this with legit wetbox installation
+} catch( e ) {
+  WET = window.WET = require( 'wetbox' )
+}
+let THREE = window.THREE = require( 'three' )
+let game = window.game = new WET.Game( THREE )
 
-document.title = 'wiz';
-
-console.log( 'Using version:', wet.getVersion() );
-wet.keyListen( 
-  () => { return true },
-  () => { console.log( 'up' ) }, 
-  () => { console.log( 'down' ) }, 
-  () => { console.log( 'left' ) }, 
-  () => { console.log( 'right' ) }, 
-  () => { console.log( 'space' ) }, 
-  () => { console.log( 'ctrl' ) } 
-);
-
-//console.log(d)
-
-let canvasEl = document.createElement('canvas');
-canvasEl.setAttribute('id', 'canvas');
-document.body.appendChild( canvasEl );
-
-console.log(wet);
-
-let x='xxxxx';
+document.title = 'Wizzy boi'
