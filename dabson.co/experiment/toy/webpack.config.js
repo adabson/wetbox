1752aa13db323b58a40889bdcc2f7ab86a191280
Dabson <cyphlix@gmail.com>
2018-08-12 11:38:18 +1000

    and I forgot to mention that im not even welcome at the startrek convention

--- dabson.co/experiment/toy/webpack.config.js
+++ dabson.co/experiment/toy/webpack.config.js
@@ -4,9 +4,10 @@ module.exports = {
   output: {},
   mode: "development",
   plugins: [ 
+    new HtmlWebpackPlugin({template:"./index.html"}),
     new HtmlWebpackPlugin({template:"./milkydrop/circlenoshader.html"}),
     new HtmlWebpackPlugin({template:"./milkydrop/rawfeed.html"}),
     new HtmlWebpackPlugin({template:"./milkydrop/rawestfeed.html"}),
-    new HtmlWebpackPlugin({template:"./index.html"}),
+    new HtmlWebpackPlugin({template:"./milkydrop/3d.html"}),
   ]
 };
