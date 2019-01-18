4f8de53c0e22130f7075d07500d126e22924ed64
Dabson <cyphlix@gmail.com>
2019-01-18 21:10:44 +1100

    modularize draw() func

--- dabson.co/experiment/lot/node_app/sat1.js
+++ dabson.co/experiment/lot/node_app/sat1.js
@@ -17,3 +17,16 @@ let basePool = [
 	[39, 40, 41, 42, 43, 44]
 ];
 
+N = 45;
+K = 6;
+
+M = 48; // multiplier. (45*2)/2 = 15 (flattest distribution), 15*48=720 (basepool.length)
+
+let blankSlate = [];
+for(let i=0;i<48;i++) { 
+  blankSlate = blankSlate.concat(JSON.parse(JSON.stringify(basePool)));
+}
+
+//genetic algo approach again :)
+
+tiks = JSON.parse(JSON.stringify(blankSlate));
