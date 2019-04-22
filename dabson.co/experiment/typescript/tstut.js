3418b65c6a6e1932baf5dc4b8ce9b76eea555776
Dabson <cyphlix@gmail.com>
2019-04-22 11:44:06 +1000

    cool

--- dabson.co/experiment/typescript/tstut.js
+++ dabson.co/experiment/typescript/tstut.js
@@ -1,2 +1,7 @@
 var myName = "Dabson";
 var myAge = 41;
+var canVote = true;
+var anything = "dog";
+anything = 2;
+let tsEl = document.getElementById("tsStuff");
+tsEl.innerHTML = "My Name is " + myName;
--- dabson.co/experiment/typescript/tstut.ts
+++ dabson.co/experiment/typescript/tstut.ts
@@ -1,3 +1,9 @@
 var myName: string = "Dabson";
 var myAge: number = 41;
 var canVote: boolean = true;
+var anything: any = "dog";
+anything = 2;
+
+let tsEl = document.getElementById("tsStuff");
+
+tsEl.innerHTML = "My Name is " + myName;
