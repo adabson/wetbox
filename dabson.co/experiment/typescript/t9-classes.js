510a9413609c1d8e72ef1bed3186e6b31be8d66c
Dabson <cyphlix@gmail.com>
2019-04-22 13:52:32 +1000

    get / set in typescript is how java should be

--- dabson.co/experiment/typescript/t9-classes.js
+++ dabson.co/experiment/typescript/t9-classes.js
@@ -7,7 +7,19 @@ class Animal {
     ownerInfo() {
         writeLn(this.name + " is owned by " + this.owner);
     }
+    getNumOfAnimals() {
+        return Animal.numOfAnimals;
+    }
+    get weight() {
+        return this._weight;
+    }
+    set weight(weight) {
+        this._weight = weight;
+    }
 }
 Animal.numOfAnimals = 0;
 let ani = new Animal("Sir fluffkins", "Cruella De Vil");
 ani.ownerInfo();
+writeLn(ani.getNumOfAnimals());
+ani.weight = 23;
+writeLn(ani.weight);
--- dabson.co/experiment/typescript/t9-classes.ts
+++ dabson.co/experiment/typescript/t9-classes.ts
@@ -1,6 +1,8 @@
 class Animal {
 	public favFood: string;
 	static numOfAnimals: number = 0;
+	private _weight: number;
+
 	constructor(private name: string, private owner: string ){
 		Animal.numOfAnimals++;
 	}
@@ -8,7 +10,22 @@ class Animal {
 	ownerInfo() {
 		writeLn(this.name + " is owned by " + this.owner);
 	}
+
+	static function getNumOfAnimals(): number {
+		return Animal.numOfAnimals;
+	}
+
+	get weight(): number {
+		return this._weight;
+	}
+
+	set weight( weight: number ) {
+		this._weight = weight;
+	}
 }
 
 let ani = new Animal("Sir fluffkins", "Cruella De Vil");
 ani.ownerInfo();
+writeLn( ani.getNumOfAnimals() );
+ani.weight = 23;
+writeLn( ani.weight );
