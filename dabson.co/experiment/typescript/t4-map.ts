fd1f7ce28418cfc110f9580b959deccbe92b4f2a
Dabson <cyphlix@gmail.com>
2019-04-22 13:25:04 +1000

    still basic js

---
+++ dabson.co/experiment/typescript/t4-map.ts
@@ -0,0 +1,13 @@
+document.write("5+4="+(5+4)+"<br>");
+document.write("5-4="+(5-4)+"<br>");
+document.write("5*4="+(5*4)+"<br>");
+document.write("5/4="+(5/4)+"<br>");
+document.write("5%4="+(5%4)+"<br>");
+
+document.write("5 +(concat) String 2 = " + (5+'2') + "<br>");
+
+var randNum: number = 1;
+document.write("randNum++ = " + randNum++ + "<br>");
+document.write("++randNum = " + ++randNum + "<br>");
+document.write("randNum-- = " + randNum-- + "<br>");
+document.write("--randNum = " + --randNum + "<br>");
