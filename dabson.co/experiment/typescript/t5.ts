f8b15b4484f2cbdcb5f26aed1a20c8a028627fe2
Dabson <cyphlix@gmail.com>
2019-04-22 13:31:19 +1000

    optimise this shizz

---
+++ dabson.co/experiment/typescript/t5.ts
@@ -0,0 +1,8 @@
+let sampLet = 123;
+
+{
+	let sampLet = 456;
+	//document.write(sampLet + "<br>");
+}
+
+document.write(sampLet + "<br>");
---
+++ dabson.co/experiment/typescript/t6-looping.ts
@@ -0,0 +1,5 @@
+var randArray = [5,6,7,8];
+
+for(var val in randArray) {
+	writeLn(val);
+}
