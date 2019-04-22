c60caf1a331d20004862f991d2f7d9cf104e15c4
Dabson <cyphlix@gmail.com>
2019-04-22 13:33:05 +1000

    for..in vs for..of

--- dabson.co/experiment/typescript/t6-looping.js
+++ dabson.co/experiment/typescript/t6-looping.js
@@ -1,4 +1,10 @@
 var randArray = [5, 6, 7, 8];
+// for(x in y) prints INDEXES
 for (var val in randArray) {
+    writeLn(val);
+}
+var strArray = randArray.map(String);
+// for(x of y) prints VALUES
+for (var val of strArray) {
     writeLn(val);
 }
--- dabson.co/experiment/typescript/t6-looping.ts
+++ dabson.co/experiment/typescript/t6-looping.ts
@@ -1,5 +1,13 @@
 var randArray = [5,6,7,8];
 
+// for(x in y) prints INDEXES
 for(var val in randArray) {
+	writeLn(val);
+}
+
+var strArray = randArray.map(String);
+
+// for(x of y) prints VALUES
+for(var val of strArray) {
 	writeLn(val);
 }
