95db999ed57fff9879d52ad55b0366e6f658bada
Dabson <cyphlix@gmail.com>
2019-04-22 12:49:53 +1000

    typeOf

--- dabson.co/experiment/typescript/tstut.js
+++ dabson.co/experiment/typescript/tstut.js
@@ -5,3 +5,7 @@ var anything = "dog";
 anything = 2;
 let tsEl = document.getElementById("tsStuff");
 tsEl.innerHTML = "My Name is " + myName;
+document.write("myName is a " + typeof (myName) + "<br>");
+document.write("canVote is a " + typeof (canVote) + "<br>");
+document.write("myAge is a " + typeof (myAge) + "<br>");
+document.write("anything is a " + typeof (anything) + "<br>");
--- dabson.co/experiment/typescript/tstut.ts
+++ dabson.co/experiment/typescript/tstut.ts
@@ -7,3 +7,8 @@ anything = 2;
 let tsEl = document.getElementById("tsStuff");
 
 tsEl.innerHTML = "My Name is " + myName;
+
+document.write("myName is a " + typeof (myName) + "<br>");
+document.write("canVote is a " + typeof (canVote) + "<br>");
+document.write("myAge is a " + typeof (myAge) + "<br>");
+document.write("anything is a " + typeof (anything) + "<br>");
