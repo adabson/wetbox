c875f7fcf6ad7c6b03d8fb6dee3e2a84cb4b3ec0
Dabson <cyphlix@gmail.com>
2025-02-09 14:58:47 +1100

    vite prefix needed?

--- dabson.co/experiment/daco/vite.config.js
+++ dabson.co/experiment/daco/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://dabson.co/experiment/daco/dist',
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [
     vue(),
     vueDevTools(),
